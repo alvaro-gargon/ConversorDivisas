43e7c726be4239659e29552d3879b4ac9b707de7
alvaro <alvaro.garcia.daw@gmail.com>
2026-05-16 20:06:30 +0200

    clean - limpieza de los archivos de debug

--- resources/js/views/viewVistaPrincipal.vue
+++ resources/js/views/viewVistaPrincipal.vue
@@ -1,15 +1,7 @@
 <template>
     <h1>Hola</h1>
-    <p>{{ token }}</p>
 </template> 
 <script>
-export default {
-        data() {
-            return {
-            token: localStorage.getItem('token')
-            }
-        }
-    }
 </script>
 <style scoped>
 </style>
